0077ffbf731e21585d1620f7d5a39be21522441a
mohamed <mohamedosman@yahoo.com>
2022-02-20 02:37:27 +0200

    adding new property page

--- src/pages/Addproperty.js
+++ src/pages/Addproperty.js
@@ -16,24 +16,48 @@ function Addprop(){
       <Row className="mt-5" >
           <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg" style={{backgroundColor:"white",opacity:0.85}}>
               <Form>
+              <Form.Group >
+                      <Form.Label>Building Number</Form.Label>
+                      <Form.Control type="text" placeholder="Enter Building Number " />
+                  </Form.Group>
+                  <br/>
                   <Form.Group >
                       <Form.Label>Street</Form.Label>
                       <Form.Control type="text" placeholder="Enter Street Name " />
                   </Form.Group>
                   <br/>
                   
-                  <Form.Group >
-                      <Form.Label>Governorate</Form.Label>
-                      <br/>
-                          <select id="inputState" classname="form-control">
-                            <option value="Cairo">Cairo</option>
-                <option value="Behaira">Behaira</option>
-                <option value="Giza">Giza</option>
-                <option value="Minia">Minia</option>
-                <option value="Aswan">Aswan</option>
-</select>
-                  </Form.Group>
+               
+                  <Form.Group controlId="formBasicSelect">
+     <Form.Label>Governorate</Form.Label>
+     <Form.Control as="select" >
+     <option value="" disabled hidden selected>Select the Governorate</option>
+     <option value="Cairo">Cairo</option>
+                            <option value="Behaira">Behaira</option>
+                            <option value="Giza">Giza</option>
+                            <option value="Minia">Minia</option>
+                            <option value="Aswan">Aswan</option>
+
+     </Form.Control>
+     
+ </Form.Group>
+ <br/>
+ <Form.Group controlId="formBasicSelect">
+     <Form.Label>Property Type</Form.Label>
+     <Form.Control as="select" >
+     
+     <option value="" disabled hidden selected>Select Property Type</option>
+              <option value="room">Room</option>
+              <option value="flat">Flat</option>
+              <option value="house">House</option>
+              <option value="villa">Villa</option>
+
+     </Form.Control>
+     
+ </Form.Group>
+                  
                   <br/>
+
                   <Form.Group >
                       <Form.Label>Price</Form.Label>
                       <Form.Control type="number" placeholder="Property Price " />
@@ -43,6 +67,11 @@ function Addprop(){
                   <Form.Group >
                       <Form.Label>Size</Form.Label>
                       <Form.Control type="text" placeholder="Property Size " />
+                  </Form.Group>
+                  <br/>
+                  <Form.Group >
+                      <Form.Label>Bedrooms</Form.Label>
+                      <Form.Control type="number" placeholder="Number of Bedrooms   " />
                   </Form.Group>
                   <br/>
                   <Form.Group >
