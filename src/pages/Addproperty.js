e513c5f6523ad5e9b8e5ab84209b7ba13bf8efb9
Khaled El-Masry <16371159+Khaled-elmasry@users.noreply.github.com>
2022-02-20 21:05:02 +0200

    AddPropery Page-Osman

--- src/pages/Addproperty.js
+++ src/pages/Addproperty.js
@@ -1,89 +1,94 @@
 import React from "react";
-import { Container, Button, Col, Form, Row } from "react-bootstrap";
-// import "./Login.css";
-import bg from "../resources/addpropimages/new.svg";
-
-import axios from 'axios';
-
-function Addprop() {
-
-  return (
+import { Container ,Button,Col,Form,Row } from "react-bootstrap"; 
+import "./Login.css";
+import bg from "../../images/bg.jpeg";
 
 
+function Addprop(){
 
-    <>
-      <Container>
-        <Row className="mt-5">
-          <Col lg={4} md={6} sm={12} className="text-center mt-5 p-3">
-            <h1 style={{ color: "purple" }}>Add property </h1>
-            <Form>
+    return(
 
 
+<div style={{ backgroundImage: `url(${bg})` }}>
+      <Container >
+        <div></div>
+      <h1 className="shadow-sm text-primary mt-5 p-3 text-center rounded">Add Property</h1>
+      <Row className="mt-5" >
+          <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg" style={{backgroundColor:"white",opacity:0.85}}>
+              <Form>
+              <Form.Group >
+                      <Form.Label>Building Number</Form.Label>
+                      <Form.Control type="text" placeholder="Enter Building Number " />
+                  </Form.Group>
+                  <br/>
+                  <Form.Group >
+                      <Form.Label>Street</Form.Label>
+                      <Form.Control type="text" placeholder="Enter Street Name " />
+                  </Form.Group>
+                  <br/>
+                  
+               
+                  <Form.Group controlId="formBasicSelect">
+     <Form.Label>Governorate</Form.Label>
+     <Form.Control as="select" >
+     <option value="" disabled hidden selected>Select the Governorate</option>
+     <option value="Cairo">Cairo</option>
+                            <option value="Behaira">Behaira</option>
+                            <option value="Giza">Giza</option>
+                            <option value="Minia">Minia</option>
+                            <option value="Aswan">Aswan</option>
 
-              <div classname="form-row">
-                <div classname="form-group col-md-6">
-                  <label >Seller name </label>
-                  <input type="text" classname="form-control" placeholder="Enter Your Name " />
-                </div>
-                <div classname="form-group col-md-6">
-                  <label >Area</label>
-                  <input type="text" classname="form-control" placeholder="Area of The Property" />
-                </div>
-              </div>
-              <div classname="form-group">
-                <label for="inputAddress">Street</label>
-                <input type="text" classname="form-control" placeholder="1234 Main St" />
-              </div>
-              <div classname="form-group">
-                <label >Price</label>
-                <input type="text" classname="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-              </div>
-              <div classname="form-row">
-                <div classname="form-group col-md-6">
-                  <label for="inputCity">City</label>
-                  <input type="text" classname="form-control" id="inputCity" />
-                </div>
-                <div classname="form-group col-md-4">
-                  <label for="inputState">Governorate</label>
-                  <select id="inputState" classname="form-control">
-                    <option value="Cairo">Cairo</option>
-                    <option value="Behaira">Behaira</option>
-                    <option value="Giza">Giza</option>
-                    <option value="Minia">Minia</option>
-                    <option value="Aswan">Aswan</option>
-                  </select>
-                </div>
+     </Form.Control>
+     
+ </Form.Group>
+ <br/>
+ <Form.Group controlId="formBasicSelect">
+     <Form.Label>Property Type</Form.Label>
+     <Form.Control as="select" >
+     
+     <option value="" disabled hidden selected>Select Property Type</option>
+              <option value="room">Room</option>
+              <option value="flat">Flat</option>
+              <option value="house">House</option>
+              <option value="villa">Villa</option>
 
-              </div>
-              <div classname="form-group">
-                <div classname="form-check">
-                  <label  >
-                    Description
-                  </label>
-                  <textarea id='desc' placeholder='write decription   ' cols='48' rows='5' required ></textarea>
+     </Form.Control>
+     
+ </Form.Group>
+                  
+                  <br/>
 
-                </div>
-              </div>
-
-
-
-              <Button variant="success" onClick={()=> {window.location.href = 'https://google.com/';} }>
-                Add Property
-              </Button>
-
-
-            </Form>
+                  <Form.Group >
+                      <Form.Label>Price</Form.Label>
+                      <Form.Control type="number" placeholder="Property Price " />
+                  </Form.Group>
+                  
+                  <br/>
+                  <Form.Group >
+                      <Form.Label>Size</Form.Label>
+                      <Form.Control type="text" placeholder="Property Size " />
+                  </Form.Group>
+                  <br/>
+                  <Form.Group >
+                      <Form.Label>Bedrooms</Form.Label>
+                      <Form.Control type="number" placeholder="Number of Bedrooms   " />
+                  </Form.Group>
+                  <br/>
+                  <Form.Group >
+                      <Form.Label>Description</Form.Label>
+                      <Form.Control as="textarea" rows={3}  placeholder='write a description '/>
+                  </Form.Group>
+                  <br/>
+                  <Button variant="primary btn-block" type="submit">
+                      Add New Property
+                  </Button>
+              </Form>
           </Col>
-          <Col lg={4} md={6} sm={12}>
-            <img className="w-100 " src={bg} alt="background"></img>
-
-
-          </Col>
-        </Row >
-      </Container>
-
-    </>
-  )
+      </Row>
+      
+  </Container>
+</div>
+    )
 
 
 
